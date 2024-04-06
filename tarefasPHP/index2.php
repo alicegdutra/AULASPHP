e9cc4422abc9c537b8528fab62371681112766ba
alicegdutra <alicegzdutra@gmail.com>
2024-04-06 10:30:00 -0300

    exercício 2

--- tarefasPHP/index2.php
+++ tarefasPHP/index2.php
@@ -5,11 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+
     <form action="resposta.php" method="post">
-        <label>Informe o valor: </label> <!--o formulário que envia as informações para a outra pasta -->
-        <input type="number" name="valor">
+        <p>
+        <label>Informe o valor 1:</label>
+        <input type="text" name="valor1">
+        </p>
+        <label>Informe o valor 2:</label>
+        <input type="text" name="valor2">
+        <p>
+        <label>Informe o valor 3:</label>
+        <input type="text" name="valor3">
+        </p>
+        <label>Informe o valor 4:</label>
+        <input type="text" name="valor4">
+        <p>
+        <label>Informe o valor 5:</label>
+        <input type="text" name="valor5">
+        </p>
+        <label>Informe o valor 6:</label>
+        <input type="text" name="valor6">
+        <p>
+        <label>Informe o valor 7:</label>
+        <input type="text" name="valor7">
+        </p>
         <button type="submit">Enviar</button>
     </form>
+
 </body>
 </html>
